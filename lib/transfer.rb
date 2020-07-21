61b65199e3faba6aeffccaadb306334d2aa29266
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-21 03:41:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && self.status != "complete" && self.status != "rejected"
+    if valid? && self.status == "pending"
       sender.balance -= @amount
       receiver.balance += @amount
       self.status = "complete"
